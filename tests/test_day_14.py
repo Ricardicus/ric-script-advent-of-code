a2e99e23a900d05a2585f494a3e27a1f983a92f4
= <rickard_hallerback@hotmail.com>
2022-12-03 08:14:54 +0100

    day 14

---
+++ tests/test_day_14.py
@@ -0,0 +1,19 @@
+import os
+from settings import *
+
+def test_day_1():
+  os.chdir("../2021")
+  output_lines = [
+"Part 1: 1588",
+"Part 2: 2188189693529"
+]
+
+  lib_script = callSample('14.ric')
+
+  ric_result = os.popen(lib_script).read().splitlines()
+
+  assert len(output_lines) == len(ric_result)
+
+  for i in range(0,len(ric_result)):
+    assert output_lines[i] == ric_result[i], "denna: " + output_lines[i] + ", verkligt: " + ric_result[i]
+
